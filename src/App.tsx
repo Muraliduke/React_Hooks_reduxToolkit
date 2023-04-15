9eca9aeedb57c5f240897aa19e6cb10453dca198
Murali <muraliduke4461@gmail.com>
2023-04-15 21:20:27 +0530

    Add router and store update

--- src/App.tsx
+++ src/App.tsx
@@ -2,55 +2,28 @@ import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
+import { News } from './features/news/News';
+import { Search } from './features/search/Search';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ResponsiveDrawer from './common/Demo';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+      <BrowserRouter>
+        <ResponsiveDrawer>
+          <Routes>
+            <Route path="/" element={<News />} />
+            <Route path="news" element={<News />} />
+            <Route path="counter" element={<Counter />} />
+            <Route path="search" element={<Search />} />
+            <Route path="*" element={<h1>Route does not exist</h1>} />
+          </Routes>
+        </ResponsiveDrawer>
+      </BrowserRouter>
+
     </div>
   );
 }
--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,14 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import newsReducer from '../features/news/newsSlice';
+import searchReducer from '../features/search/searchSlice';
+
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    news: newsReducer,
+    search: searchReducer,
   },
 });
 
